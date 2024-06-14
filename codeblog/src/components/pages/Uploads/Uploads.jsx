1e0150d103c1804a921db8ba67368b91e9da089d
Aditya Gupta <121934082+Adityagupta930@users.noreply.github.com>
2024-06-14 22:53:31 +0530

    uploads problem solved

--- codeblog/src/components/pages/Uploads/Uploads.jsx
+++ codeblog/src/components/pages/Uploads/Uploads.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Upload from '../../specific/Upload/Upload'
-import blogs from '../../../uploadBlogs.json'
-
+import React from 'react';
+import Upload from '../../specific/Upload/Upload';
+import blogs from '../../../uploadBlogs.json';
 
 function Uploads() {
   console.log("Uploads.jsx rendered");
   return (
     <>
-    {blogs.map((blog) => (
-      <Upload 
-        blogs={blog.blogsList} 
-        month={blog.month} 
-      />
-    ))}
+      {blogs.map((blog, index) => (
+        <Upload 
+          key={index}
+          blogs={blog.blogsList} 
+          month={blog.month} 
+        />
+      ))}
     </>
-  )
+  );
 }
 
-export default Uploads
+export default Uploads;
